5f79fb8ea0c758eb399f6a5f727b46c74896c3d5
zaltechdev <hanifzalfa910@gmail.com>
2025-07-22 15:12:10 +0700

    add aos library

--- sources/Core/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// use App\Controllers\TestController;
-// use App\Middlewares\TestMiddleware;
-
-// $app->get("/", [TestController::class,"index"], [
-//     [TestMiddleware::class,"index"]
-// ]);
